ab174752b43bda97d79127cc587cf6b0182239a2
ptoraska <ptoraska@redhat.com>
2017-08-22 15:20:48 +0530

    add main code

--- wsgi.py
+++ wsgi.py
@@ -8,11 +8,14 @@ BACKEND_PROTOCOL = 'rpc://'
 
 CELERY_APP = Celery('celery_task', broker=BROKER_URL, backend=BACKEND_PROTOCOL)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
-
 application.route("/celerytest")
 def hello():
     res = CELERY_APP.send_task('dwm_task.dwm_post_process', ('Test_Batch_ID'))
     return "Task Submitted Successfully!"
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    application.run()
