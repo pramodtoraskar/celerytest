5f838e6e06dcc31e1ee9c488bd87bcdfca95d1fd
ptoraska <ptoraska@redhat.com>
2017-08-22 15:09:10 +0530

    add celery code

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from celery import Celery
+
 application = Flask(__name__)
+
+BROKER_URL = 'amqp://guest:guest@rabbitmq:5672'
+BACKEND_PROTOCOL = 'rpc://'
+
+CELERY_APP = Celery('celery_task', broker=BROKER_URL, backend=BACKEND_PROTOCOL)
 
 @application.route("/")
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
-    application.run()
+application.route("/celerytest")
+def hello():
+    res = CELERY_APP.send_task('dwm_task.dwm_post_process', ('Test_Batch_ID'))
+    return "Task Submitted Successfully!"
